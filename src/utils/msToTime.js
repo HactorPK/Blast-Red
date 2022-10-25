bae17660b1d368ef646051b9c827359cb81966e7
HactorPK <101172691+HactorPK@users.noreply.github.com>
2022-10-25 19:24:09 +0200

    Delete src directory

--- src/utils/msToTime.js
+++
@@ -1,9 +0,0 @@
-export default function msToTime(seconds) {
-  let ms = seconds % 1000;
-  let s = (seconds - ms) / 1000;
-  let secs = s % 60;
-  secs = (secs < 10) ? `0${secs}` : secs;
-  s = (s - secs) / 60;
-  let mins = s % 60;
-  return mins + ":" + secs;
-}
